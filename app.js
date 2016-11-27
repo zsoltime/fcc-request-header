9ebb0d50812680da9309f2cd1cf634a5bc06415b
= <zsolt@sowad.com>
2016-11-27 22:50:31 +0000

    Updates

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-"use strict"
-var express = require("express");
+'use strict'
+var express = require('express');
 var app = express();
 
-app.set("port", process.env.PORT || 8000);
-app.set("views", (__dirname + "/views"));
-app.set("view engine", "jade");
-app.use(express.static(__dirname + "/public"));
+app.set('port', process.env.PORT || 8000);
+app.set('views', (__dirname + '/views'));
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
 
-app.get("/api/whoami", function(req, res) {
-    res.contentType("application/json");
+app.get('/api/whoami', function(req, res) {
+    res.contentType('application/json');
     res.status(200).json({
         ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
-        language: req.headers["accept-language"].match(/^(.*?),/)[1],
-        software: req.headers["user-agent"].match(/\((.*?)\)/)[1]
+        language: req.headers['accept-language'].match(/^(.*?),/)[1],
+        software: req.headers['user-agent'].match(/\((.*?)\)/)[1]
     });
 });
 
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
     var sample = {
-        ipaddress: "86.205.25.192",
-        language: "en-GB",
-        software: "Macintosh; Intel Mac OS X 10_11_4"
+        ipaddress: '86.205.25.192',
+        language: 'en-GB',
+        software: 'Macintosh; Intel Mac OS X 10_11_4'
     };
     var data = {
         sample: JSON.stringify(sample, null, 2)
     };
-    res.render("index", data);
+    res.render('index', data);
 });
 
-var server = app.listen(app.get("port"), function() {
-    console.log("The app is running on http://localhost: %s", server.address().port);
+var server = app.listen(app.get('port'), function() {
+    console.log('The app is running on http://localhost: %s', server.address().port);
 });
