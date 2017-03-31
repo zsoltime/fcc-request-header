3aa31232c0c74241484e9924ef319e6d365fc7ac
Zsolt Meszaros <zsolt@sowad.com>
2017-03-31 10:18:41 +0100

    Update indentation

--- app.js
+++ app.js
@@ -8,26 +8,26 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/api/whoami', function(req, res) {
-    res.contentType('application/json');
-    res.status(200).json({
-        ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
-        language: req.headers['accept-language'].match(/^(.*?),/)[1],
-        software: req.headers['user-agent'].match(/\((.*?)\)/)[1]
-    });
+  res.contentType('application/json');
+  res.status(200).json({
+    ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+    language: req.headers['accept-language'].match(/^(.*?),/)[1],
+    software: req.headers['user-agent'].match(/\((.*?)\)/)[1]
+  });
 });
 
 app.get('/', function(req, res) {
-    var sample = {
-        ipaddress: '86.205.25.192',
-        language: 'en-GB',
-        software: 'Macintosh; Intel Mac OS X 10_11_4'
-    };
-    var data = {
-        sample: JSON.stringify(sample, null, 2)
-    };
-    res.render('index', data);
+  var sample = {
+    ipaddress: '86.205.25.192',
+    language: 'en-GB',
+    software: 'Macintosh; Intel Mac OS X 10_11_4'
+  };
+  var data = {
+    sample: JSON.stringify(sample, null, 2)
+  };
+  res.render('index', data);
 });
 
 var server = app.listen(app.get('port'), function() {
-    console.log('The app is running on http://localhost: %s', server.address().port);
+  console.log('The app is running on http://localhost: %s', server.address().port);
 });
